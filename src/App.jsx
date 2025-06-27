e0a74a11160f32409182fdf36ddb40267cfbb4a9
MistuoK05 <sumitkalebere@gmail.com>
2025-06-27 15:36:05 +0530

    Third update commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,36 @@ import ProfilePopup from "./pages/ProfilePopup";
 import './styles/Popup.css';
 import ApplyPage from "./pages/ApplyPage";
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
 
 
 const App =() =>{
-  const [showPopup,setShowPopup]=useState(true);
-  
-  const handlePopupClose=()=>{
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
     setShowPopup(false);
-  }
+  };
+  // const [showPopup,setShowPopup]=useState(true);
+  
+  // const handlePopupClose=()=>{
+  //   setShowPopup(false);
+  // }
+
   return(
+    <>
     <div>
-     
-            { showPopup &&
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Sumit Sunil Kalebere"
+          studentPhotoUrl="/images/profile-img.png" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+    <div>
+            {/* { showPopup &&
               <ProfilePopup handleClose={handlePopupClose} />
-            }
+            } */}
             <Router>
               <Routes>
               <Route path="/" element={<HomePage/>}/>
@@ -35,10 +51,8 @@ const App =() =>{
             </Routes>
             <ChatbotComponent/>
       </Router>
-            
-          
-      
     </div>
+    </>
   )
 }
 export default App;
